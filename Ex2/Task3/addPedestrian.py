34e6ff8e71c44481193748e45a25d8bffc27f6fa
Cyr-Ch <cyrine.chaabani19@gmail.com>
2020-12-01 16:46:10 +0100

    Add documentation to addPedestrian.py

--- Ex2/Task3/addPedestrian.py
+++ Ex2/Task3/addPedestrian.py
@@ -4,6 +4,10 @@ import numpy as np
 
 
 def generate_default_pedestrian():
+    """
+    Generates an initialized pedestrian object to be added to the scenario json
+    :return: an initialized pedestrian
+    """
     false = False
     null = None
     return {
@@ -59,6 +63,7 @@ def generate_default_pedestrian():
         "type": "PEDESTRIAN"
     }
 
+# Path to scenario to be modified
 path_to_scenario = "/content/drive/MyDrive/RiMEA6.scenario"
 # read file
 with open(path_to_scenario, 'r') as myfile:
@@ -68,9 +73,12 @@ with open(path_to_scenario, 'r') as myfile:
 obj = json.loads(data)
 obj["name"] = obj["name"]+"_new"
 
+# calculate new x and y coordinates
 new_pedestrian_coord_y = obj["scenario"]["topography"]["sources"][0]["shape"]["y"] + obj["scenario"]["topography"]["sources"][0]["shape"]["height"]/2
 new_pedestrian_coord_x = obj["scenario"]["topography"]["targets"][0]["shape"]["x"] + obj["scenario"]["topography"]["targets"][0]["shape"]["width"]/2
 new_pedestrian = generate_default_pedestrian()
+
+# Find already used pedestrian Ids to avoid confusions
 ids = []
 for dynamicattr in obj["scenario"]["topography"]["dynamicElements"]:
     if dynamicattr["type"] == "PEDESTRIAN":
@@ -79,13 +87,19 @@ if len(ids) > 0:
   max_id = np.max(ids)
 else:
   max_id = 0
+# Find target id  
 targetid = obj["scenario"]["topography"]["sources"][0]["targetIds"]
+# Set new pedestrian's target id
 new_pedestrian["targetIds"] = targetid
+# Set new pedestrian's id
 new_pedestrian["attributes"]["id"] = int(max_id + 1)
+# Set new pedestrian's x and y coordinates
 new_pedestrian["position"]["x"] = new_pedestrian_coord_x
 new_pedestrian["position"]["y"] = new_pedestrian_coord_y
+# Add pedestrian to the scenario's dictionary
 obj["scenario"]["topography"]["dynamicElements"].append(new_pedestrian)
 
+# Save to a new json file
 with open(path_to_scenario.replace(".scenario","_new.scenario"), 'w') as outfile:
     json.dump(obj, outfile, indent=4)
 
